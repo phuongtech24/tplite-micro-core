0c437d2f1f9221c9fb2c195a00a11ab8086a1f4b
phuongtech24 <namphuong2462@gmail.com>
2026-08-03 14:24:49 +0700

    feat:cấu hình api gateway

--- api-gateway/src/main/java/com/tplite/banking/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tplite/banking/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
-﻿package com.tplite.banking.apigateway;
+package com.tplite.banking.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
